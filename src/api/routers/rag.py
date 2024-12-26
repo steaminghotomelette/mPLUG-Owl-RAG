e7d4a7c8cfbc0451f4d7868d0d97bdbc1e6ef36f
Lim Yi Xuan <limyixuan12345@gmail.com>
2024-12-27 01:37:04 +0800

    Support multiple uploads + cleanup RAG UI code.

--- src/api/routers/rag.py
+++ src/api/routers/rag.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, UploadFile, File, Form, HTTPException
 from typing import Dict, List
 from api.routers import summarizer, chunker
 from db.utils import USER_COLLECTION_NAME
-from utils.rag_utils import RAGManager, switch_model, rag_search
+from utils.rag_utils import RAGManager, switch_model, rag_search, UPLOAD_ALLOWED_TYPES
 
 # Initialize API router
 router = APIRouter(
@@ -21,76 +21,63 @@ rag = RAGManager()
 # --------------------------------------------
 @router.post("/upload")
 async def upload_document_to_rag(
-    document: UploadFile = File(...),
-    metadata: str = Form(...),
+    files: List[UploadFile] = File(None),
+    metadata: List[str] = Form(...),
     embedding_model: str = Form(...)
 ) -> Dict:
     """
     Upload a file, extract embeddings, and store it in the database.
 
     Args:
-        document (UploadFile): File uploaded.
-        metadata (str): Metadata associated with the file.
+        files (List[UploadFile]): List of files to be uploaded.
+        metadata (List[str]): Metadata associated with each uploaded file.
         embedding_model (str): Embedding model to use for the file.
 
     Returns:
         dict: Success message or error details.
     """
-    text_embeddings_list = None
-    img_embeddings_list = None
-    raw_image = None
-    text_content = f"{metadata}"
     data = []
     try:
         switch_model(rag, embedding_model)
-        # get the file bytes
-        file_content = await document.read()
-        # Validate file type
-        allowed_types = [
-            "application/pdf", "image/png", "image/jpeg",
-            "video/mp4", "image/gif", "video/x-msvideo"
-        ]
-        if document.content_type not in allowed_types:
-            raise HTTPException(
-                status_code=400, detail="Unsupported file type.")
         collection_name = f"{USER_COLLECTION_NAME}_{rag.embedding_model_manager.model_type.value}"
 
-        # Extract embeddings based on file type
-        if document.content_type in ["image/png", "image/jpeg", "image/gif"]:
-            img_embeddings_list, raw_image = rag.embedding_model_manager.embed_image(
-                file_content)
-            text_embeddings_list = rag.embedding_model_manager.embed_text(
-                text_content)
-        else:
-            # Chunk and summarize text from PDF
-            try:
-                chunk_text, chunk_metadata = chunker.split_document(file_content,
-                                                                    max_size=200, chunk_overlap=20)
-                text_content = summarizer.contextualize_chunks(
-                    chunk_text, os.getenv('GEMINI_API_KEY'))
-            except Exception as e:
-                raise Exception(f"Failed to extract PDF text: {str(e)}")
+        for i, file in enumerate(files):
+            if file.content_type not in UPLOAD_ALLOWED_TYPES:
+                raise HTTPException(
+                    status_code=400, detail="Unsupported file type.")
+            
+            # get the file bytes
+            file_content = await file.read()
 
-            for text in text_content:
-                text_embeddings_list = rag.embedding_model_manager.embed_text(
-                    text)
+            # Extract embeddings based on file type
+            if file.content_type in ["image/png", "image/jpeg", "image/gif"]:
+                img_embeddings_list, raw_image = rag.embedding_model_manager.embed_image(file_content)
+                text_embeddings_list = rag.embedding_model_manager.embed_text(metadata[i])
                 data.append({
-                    "text": text,
+                    "text": metadata[i],
                     "text_embedding": text_embeddings_list,
                     "image_embedding": img_embeddings_list,
                     "image_data": raw_image,
-                    "metadata": metadata,
+                    "metadata": metadata[i],
                 })
 
-        # Prepare the insertion payload
-        if len(data) == 0:
-            data.append({
-                "text": text_content,
-                "text_embedding": text_embeddings_list,
-                "image_embedding": img_embeddings_list,
-                "image_data": raw_image,
-                "metadata": metadata,
-            })
+            else:
+                # Chunk and summarize text from PDF
+                try:
+                    chunk_text, chunk_metadata = chunker.split_document(file_content, max_size=200, chunk_overlap=20)
+                    text_content = summarizer.contextualize_chunks(chunk_text, os.getenv('GEMINI_API_KEY'))
+                except Exception as e:
+                    raise Exception(f"Failed to extract PDF text: {str(e)}")
+
+                for text in text_content:
+                    text_embeddings_list = rag.embedding_model_manager.embed_text(text)
+                    data.append({
+                        "text": text,
+                        "text_embedding": text_embeddings_list,
+                        "image_embedding": None,
+                        "image_data": None,
+                        "metadata": metadata[i],
+                    })
 
         # Insert the data into the database
         response = rag.db.insert(collection_name, data)
@@ -98,8 +85,7 @@ async def upload_document_to_rag(
         return response
 
     except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Failed to upload file: {str(e)}")
+        raise Exception(f"Failed to upload file: {e}")
 
 
 @router.post("/reset_rag")
